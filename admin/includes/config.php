300b96e6621802f6a9dc47b6eb443deb3fe2fd4a
jho9223 <148685216+jho9223@users.noreply.github.com>
2025-12-15 15:06:36 +0800

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,6 +1,6 @@
 <?php 
 // DB credentials.
-define('DB_HOST','ibrahimlibrarysystem.netlify.app');
+define('DB_HOST','https://library-management-system-a3ws.onrender.com');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','library');
